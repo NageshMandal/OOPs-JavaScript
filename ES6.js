c4c3be30f4beb7ce11ddec101d62bead347404c9
NageshMandal <106006006+NageshMandal@users.noreply.github.com>
2022-10-26 17:10:10 +0530

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -13,32 +13,44 @@
 
 
 
-// const myButton = document.querySelector('#myButton');
+const myButton = document.querySelector('#myButton');
 
-// const shop = {
+const shop = {
     
-//     price: 100,
+    price: 100,
 
-//     buy: function () {
+    buy: function () {
 
-//         // const self = this;
-//                                        //function
-//         myButton.addEventListener('click',() => {
+        // const self = this;
+                                       //function
+        myButton.addEventListener('click',() => {
 
-//             document.write('I Spend' + this.price);
-//         });
-//     },
-// };
-// shop.buy();
+            document.write('I Spend' + this.price);
+        });
+    },
+};
+shop.buy();
+
+
 
 // Tamplates literals AKA Backticks
 
-const name = 'Nagesh';
+// const name = 'Nagesh';
 
-const message = `hello ${name}
-how are you
-`;
-document.write(message);
+// const message = `hello ${name}
+// how are you
+// `;
+// document.write(message);
+
+
+// Enhanced object literals
+
+// computed property key
+
+// const product = {
+//     name
+// }
+
 
 
 
