bf70491db4696d3a02536b51fe4ee2c39e7a9d49
NageshMandal <106006006+NageshMandal@users.noreply.github.com>
2022-10-26 11:36:33 +0530

    .

--- ES6.js
+++ ES6.js
@@ -1,11 +1,34 @@
-const addition = (a, b) => {
-    document.write(a+b);
+// const addition = (a, b) => {
+//     document.write(a+b);
+// };
+
+// addition(9 ,5);
+
+// // arrow function
+
+// const add = (a,b) => document.write(a+b);
+
+// add(9,8);
+
+const myButton = document.querySelector('#myButton');
+
+const shop = {
+    
+    price: 100,
+
+    buy: function () {
+
+        // const self = this;
+
+        myButton.addEventListener('click', () => {
+
+            document.write('I Spend' + this.price);
+        });
+    },
 };
+shop.buy();
 
-addition(9 ,5);
 
-// arrow function
 
-const add = (a,b) => document.write(a+b);
 
-add(9,8);
+
