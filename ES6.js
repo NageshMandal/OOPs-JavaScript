55e1b19175cdd61f323f9c2e1d7cd53dc499695a
NageshMandal <106006006+NageshMandal@users.noreply.github.com>
2022-10-26 11:38:56 +0530

    .

--- ES6.js
+++ ES6.js
@@ -1,14 +1,17 @@
-// const addition = (a, b) => {
-//     document.write(a+b);
-// };
+const addition = (a, b) => {
+    document.write(a+b);
+};
 
-// addition(9 ,5);
+addition(9 ,5);
 
-// // arrow function
+// arrow function
 
-// const add = (a,b) => document.write(a+b);
+const add = (a,b) => document.write(a+b);
 
-// add(9,8);
+add(9,8);
+
+
+
 
 const myButton = document.querySelector('#myButton');
 
@@ -19,8 +22,8 @@ const shop = {
     buy: function () {
 
         // const self = this;
-
-        myButton.addEventListener('click', () => {
+                                       //function
+        myButton.addEventListener('click',() => {
 
             document.write('I Spend' + this.price);
         });
@@ -28,7 +31,7 @@ const shop = {
 };
 shop.buy();
 
-
+// Tamplates literals AKA Backticks
 
 
 
